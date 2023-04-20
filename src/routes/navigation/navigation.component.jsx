6e6c7a11e8fb792be9c1e349c0def234cdcbaf0b
Muhammad-Sabbir <md.sabbirahamedtushar@gmail.com>
2023-04-21 00:35:35 +0600

    Chapter 05: Routing ReactRouter>> Lecture 86: React Router Link

---
+++ src/routes/navigation/navigation.component.jsx
@@ -0,0 +1,20 @@
+import { Fragment } from 'react';
+import { Outlet, Link } from 'react-router-dom';
+const Navigation = () => {
+  return (
+    <Fragment>
+      <div className='navigation'>
+        <Link className='logo-container' to='/'>
+          <div>Logo</div>
+        </Link>
+        <Link className='nav-link' to='/shop'>
+          SHOP
+        </Link>
+        <div className='nav-links-container'></div>
+      </div>
+      <Outlet />
+    </Fragment>
+  );
+};
+
+export default Navigation;
