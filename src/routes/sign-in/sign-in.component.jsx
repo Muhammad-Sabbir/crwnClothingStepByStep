310a3c646bd54915709a9d8648cad9a0628a80e4
Muhammad-Sabbir <md.sabbirahamedtushar@gmail.com>
2023-04-23 21:33:58 +0600

    Chapter 06: Authentication Firebase>> Lecture 96: Sign In With Redirect

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,17 +1,52 @@
+// // example 01: Hooks API Reference
+// import { useEffect } from 'react';
+// import { getRedirectResult } from 'firebase/auth';
+// // example 01: Hooks API Reference
+
 import {
+  // // example 01: Hooks API Reference
+  // auth,
+  // signInWithGoogleRedirect,
+  // // example 01: Hooks API Reference
   signInWithGooglePopup,
   createUserDocumentFromAuth,
 } from '../../utils/firebase/firebase.utils';
+
+// // //example 01: Hooks API Reference
+// const imperativeFunction = async () => {
+//   const response = await getRedirectResult(auth);
+//   console.log(response);
+//   if (response) {
+//     const userDocRef = await createUserDocumentFromAuth(response.user);
+//   }
+// };
+// // //example 01: Hooks API Reference
+
 const SignIn = () => {
+  // //example 01: Hooks API Reference
+  // useEffect(() => {
+  //   imperativeFunction(); // https://medium.com/geekculture/react-uncaught-typeerror-destroy-is-not-a-function-192738a6e79b
+  // }, []);
+  //  //example 01: Hooks API Reference
+
   const logGoogleUser = async () => {
     const { user } = await signInWithGooglePopup();
     const userDocRef = await createUserDocumentFromAuth(user);
   };
 
+  // here we do not use this method, because when we click the ridrect button this will bring us into new google authentication page and then redirect it back to this page again, and when we getback this page again then react will rerender this page initially and thats why our variables data will not store anything from authentication page. That's why we need to use another process. We need to import useEffect, getRiderectResult method and auth method to overcome this problem.
+  // const logGoogleRederectUser = async () => {
+  //   const { user } = await signInWithGoogleRedirect();
+  //   console.log({ user });
+  // };
+
   return (
     <div>
       <h1>Sign In Page</h1>
       <button onClick={logGoogleUser}>Sign in with Google Popup</button>
+      {/*Example01: Hooks API Reference */}
+      {/* <button onClick={signInWithGoogleRedirect}>Sign in with Google Redirect</button> */}
+      {/*Example01: Hooks API Reference */}
     </div>
   );
 };
