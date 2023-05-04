3bb7e169c27dcd2c08ed1b4a9a98d7b9ef78a529
Muhammad-Sabbir <md.sabbirahamedtushar@gmail.com>
2023-05-05 00:30:51 +0600

    Chapter 07: React Context For State Management>> Lecture 105: User Context

--- src/components/sign-in-form/sign-in-form.component.jsx
+++ src/components/sign-in-form/sign-in-form.component.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useContext } from 'react';
 
 import FormInput from '../form-input/form-input.component';
 
@@ -9,6 +9,8 @@ import {
 } from '../../utils/firebase/firebase.utils';
 
 import Button from '../button/button.component';
+
+import { UserContext } from '../../contexts/user.context';
 
 import './sign-in-form.styles.scss';
 
@@ -22,6 +24,8 @@ const SignInForm = () => {
   const { email, password } = formFields;
   console.log(formFields);
 
+  const { setCurrentUser } = useContext(UserContext);
+
   const resetFormFields = () => {
     setFormFields(defaultFormFields);
   };
@@ -34,8 +38,8 @@ const SignInForm = () => {
   const handleSubmit = async (event) => {
     event.preventDefault();
     try {
-      const response = await signInAuthUserWithEmailAndPassword(email, password);
-      console.log(response);
+      const { user } = await signInAuthUserWithEmailAndPassword(email, password);
+      setCurrentUser(user);
       resetFormFields();
     } catch (error) {
       switch (error.code) {
